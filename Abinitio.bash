7800bae6e7856608c4077a29cecc9a8493580404
Sari Sabban <sari.sabban@gmail.com>
2017-06-27 16:46:44 +0100

    Update Abinitio.bash

--- Abinitio.bash
+++ Abinitio.bash
@@ -8,26 +8,28 @@ This script was written by Sari Sabban on 27-June-2017.
 
 Required Input Files:
 ---------------------
-1. aat000_03_05.200_v1_3
-2. aat000_09_05.200_v1_3
-3. structure.pdb
-4. structure.fasta
-5. t000_.psipred_ss2
+1. aat000_03_05.200_v1_3 	---> The 3-mer fragment file
+2. aat000_09_05.200_v1_3	---> The 9-mer fragment file
+3. structure.pdb		---> The structure's PDB file
+4. structure.fasta		---> The structure's sequence file in FASTA format
+5. t000_.psipred_ss2		---> The PsiPred prediction file
 
 How To Use:
 -----------
 1. Before using this script you must make sure it works in your HPC by running each section individually, job chaining (what this script does) can disrupt the HPC if run incorrectly. There are lines in this script that only works in specific supercomputers and not others.
-2. Find all nessesary paths in this script and change them using this command:
+2. It goes without saying that you need to download and compile the Rosetta modeling software [https://www.rosettacommons.org].
+3. Identify the path to Rosetta and update this script using this command:
 	sed -i 's^{ROSETTA}^PATH/TO/ROSETTA^g' Abinitio.bash
-3. Make sure you have all the nessesary input files in the working directory.
-4. Execute this script to generate all nessesary files and folders and submit job for computation using this command:
+4. Make sure you have all the nessesary input files in the working directory.
+5. Execute this script to generate all nessesary files and folders then submit the job for computation using this command:
 	bash Abinitio.bash && qsub abinitio.pbs
 6. This script is setup to run using the PBS job scheduler, simple changes can be made to make it work on other job schedulers, but thorough understading of each job scheduler is nessesary to make these modifications.
-7. The default computatio settings are for normal Abinitio computation (25,000 structures). You will have to change the walltime for larger computations (1,000,000):
+7. The default computation settings are for normal Abinitio computation (25,000 decoys). You will have to run the following command to setup the script for larger computations (1,000,000 decoys):
 	sed -i '/#PBS -l walltime=9:00:00/d' Abinitio.bash && sed -i '/#PBS -l walltime=2:00:00/d' Abinitio.bash && sed -i 's/thin/thin_1m/g' Abinitio.bash && sed -i 's/-nstruct 25/-nstruct 1000/g' Abinitio.bash
 COMMENT
 #---------------------------------------------------------------------------------------------------------------
-echo '#!/bin/bash
+cat << 'EOF' > abinitio.pbs
+#!/bin/bash
 #PBS -N Abinitio
 #PBS -q thin
 #PBS -l walltime=9:00:00
@@ -38,7 +40,8 @@ echo '#!/bin/bash
 cd $PBS_O_WORKDIR
 {ROSETTA}/main/source/bin/AbinitioRelax.default.linuxgccrelease -database {ROSETTA}/main/database -in:file:frag3 ./aat000_03_05.200_v1_3 -in:file:frag9 ./aat000_09_05.200_v1_3 -in:file:fasta ./structure.fasta -in:file:native ./structure.pdb -psipred_ss2 ./t000_.psipred_ss2 -nstruct 1 -abinitio:relax -use_filters true -abinitio::increase_cycles 10 -abinitio::rg_reweight 0.5 -abinitio::rsd_wt_helix 0.5 -abinitio::rsd_wt_loop 0.5 -relax::fast -out:file:silent ./fold_silent_${PBS_ARRAY_INDEX}.out
 
-qsub cluster.pbs -W depend=afterokarray:${PBS_JOBID}' > abinitio.pbs
+qsub cluster.pbs -W depend=afterokarray:${PBS_JOBID}
+EOF
 
 cat << 'EOF' > cluster.pbs
 #!/bin/bash
