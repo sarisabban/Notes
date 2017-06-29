582a47112394265fea9fc654fb59b0b5ba19007e
Sari Sabban <sari.sabban@gmail.com>
2017-06-29 21:16:56 +0100

    Update Abinitio.bash

--- Abinitio.bash
+++ Abinitio.bash
@@ -40,7 +40,7 @@ cat << 'EOF' > abinitio.pbs
 cd $PBS_O_WORKDIR
 {ROSETTA}/main/source/bin/AbinitioRelax.default.linuxgccrelease -database {ROSETTA}/main/database -in:file:frag3 ./aat000_03_05.200_v1_3 -in:file:frag9 ./aat000_09_05.200_v1_3 -in:file:fasta ./structure.fasta -in:file:native ./structure.pdb -psipred_ss2 ./t000_.psipred_ss2 -nstruct 1 -abinitio:relax -use_filters true -abinitio::increase_cycles 10 -abinitio::rg_reweight 0.5 -abinitio::rsd_wt_helix 0.5 -abinitio::rsd_wt_loop 0.5 -relax::fast -out:file:silent ./fold_silent_${PBS_ARRAY_INDEX}.out
 
-qsub cluster.pbs -W depend=afterokarray:${PBS_JOBID}
+qsub -W depend=afterok:${PBS_ARRAY_ID} cluster.pbs #<---- PROBLEM HERE
 EOF
 
 cat << 'EOF' > cluster.pbs
@@ -51,8 +51,9 @@ cat << 'EOF' > cluster.pbs
 #PBS -l select=1:ncpus=1
 #PBS -j oe
 
-{ROSETTA}/main/source/bin/relax.default.linuxgccrelease -database {ROSETTA}/main/database -s ./structure.pdb -relax:thorough -nooutput -nstruct 10 -out:file:silent ./relax.out
-grep SCORE ./relax.out | awk '{print $27 "\t" $2}' > ./relax.dat
+cd $PBS_O_WORKDIR
+{ROSETTA}/main/source/bin/relax.default.linuxgccrelease -database {ROSETTA}/main/database -s ./structure.pdb -native ./structure.pdb -relax:thorough -in:file:fullatom -nooutput -nstruct 1 -out:file:silent ./relax.out
+grep SCORE ./relax.out | awk '{print $20 "\t" $2}' > ./relax.dat
 {ROSETTA}/main/source/bin/combine_silent.default.linuxgccrelease -in:file:silent ./fold_silent_*.out -out:file:silent ./fold.out
 grep SCORE ./fold.out | awk '{print $27 "\t" $2}' > ./fold.dat
 tail -n +2 "./fold.dat" > "./fold.dat.tmp" && mv "./fold.dat.tmp" "./fold.dat"
@@ -65,16 +66,16 @@ rm ./liststring
 rm ./*.fsc
 rm ./fold_silent_*
 rm ./Abinitio.o*
-mv S_* ./cluster
+mv *_*.pdb ./cluster
 cd ./cluster
-{ROSETTA}/main/source/bin/cluster.default.linuxgccrelease -database {ROSETTA}/main/database -in:file:fullatom -cluster:radius 3 -nooutput -out:file:silent ./cluster/cluster.out -in:file:s ./cluster/*.pdb
-rm ./cluster/*.pdb
-{ROSETTA}/main/source/bin/extract_pdbs.linuxgccrelease -in::file::silent ./cluster/cluster.out -out:pdb -in:file:tags
+{ROSETTA}/main/source/bin/cluster.default.linuxgccrelease -database {ROSETTA}/main/database -in:file:fullatom -cluster:radius 3 -nooutput -out:file:silent ./cluster.out -in:file:s ./*.pdb
+rm ./*.pdb
+{ROSETTA}/main/source/bin/extract_pdbs.linuxgccrelease -in::file::silent ./cluster.out -out:pdb -in:file:tags
 cd ..
-gnuplot
-set terminal postscript
+echo "set terminal postscript
 set output './plot.pdf'
 set encoding iso_8859_1
+set term post eps enh color
 set xlabel 'RMSD (\305)'
 set ylabel 'Score'
 set yrange [:-80]
@@ -82,5 +83,7 @@ set xrange [0:20]
 set title 'Abinitio Result'
 plot './fold.dat' lc rgb 'red' pointsize 0.2 pointtype 7 title '', \
 './relax.dat' lc rgb 'green' pointsize 0.2 pointtype 7 title ''
-exit
+exit" > gnuplot_sets
+gnuplot < gnuplot_sets
+rm gnuplot_sets
 EOF
