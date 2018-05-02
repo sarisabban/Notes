025f549dc7de052c20d434ddd56564150927852b
acresearch <sari.sabban@gmail.com>
2018-05-02 19:03:51 +0300

    updates

---
+++ Machine Learning/FNN.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+
+import keras , pandas , sklearn
+
+#Study
+'''
+Optimisers:		Adam Gradient Descent
+Loss function:		Cross-Entropy (if the labels are integers use sparse_categorical_crossentropy if they are one-hot encoded use categorical_crossentropy)
+Activiation:		ReLU for the hidden layers and softmax for the output layer (to give probability of different classes) or linear function (for regression)
+			Leaky ReLU (small negative slope rather than 0) is sometimes used to allow better backpropagation if a certain weight have a too large of a negative weight that causes the node to never fire. Only used if large portion of the nodes are deing, other than that always use ReLU.
+			Always end with Softmax (for single lable classifying), because it gives probability between 0 and 1
+			Always end with Sigmoid (for multi lable classifying), because it does not give probability between 0 and 1
+Neural Networks:	Feed Forward (FFNN), Recurrent (RNN & LSTM), Convolutional (CNN), Unsupervised (GAN)
+Layers:			*First layer:	1 layer, nodes = ?. The first layer also contains the input layer defined as input_shape = (number of features,) and the ,) indicates that it is a single element tuple
+			*Hidden layer:	1 - 2 layers (except for convolutional networks), nodes = ?. The starting number of nodes can be between the number of nodes in the input later and the number of nodes in the output layer. But they can increase to the 100 until good accuracy is acheived
+			*Output layer:	1 layer, nodes = number of classes 
+Learning Rate:		The steps taken in Gradient Descent to reach the global minima. low = more accurate but slower. If loss is increasing that means the Learning Rate is high. Default is always 0.01
+'''
+
+#To run computation on a GPU
+'''
+1. Install CUDA: sudo pacman -S cuda
+2. Install NUMBA: pip3 install numba
+3. from numba import vectorize , cuda
+4. @Vectorize(['float32(float32 , float32)'] , target = cuda)
+'''
+
+#Import data
+data = pandas.read_csv('fruits.csv')
+X = pandas.DataFrame.as_matrix(data[['mass' , 'width', 'height' , 'color_score']])
+Y = pandas.DataFrame.as_matrix(data['fruit_label'])
+n_class = 4
+n_featu = X.shape[1]
+Y = keras.utils.to_categorical(Y, n_class)
+X , Y = sklearn.utils.shuffle(X , Y , random_state = 0)
+
+#Setup neural network
+model = keras.models.Sequential()
+model.add(keras.layers.core.Dense(3 , input_shape = (n_featu,) , activation = 'relu'))
+model.add(keras.layers.core.Dropout(0.2))
+model.add(keras.layers.core.Dense(3 , activation = 'relu'))
+model.add(keras.layers.core.Dropout(0.2))
+model.add(keras.layers.core.Dense(n_class , activation = 'softmax'))
+
+#Compile model
+model.compile(keras.optimizers.Adam(lr = 0.01) , loss = 'categorical_crossentropy' , metrics = ['accuracy'])
+model.summary()
+
+#Train model
+model.fit(X , Y , batch_size = 8 , epochs = 100 , verbose = 2 , validation_split  = 0.25)
+
+#Save model weights to HDF5 - sudo pacman -S python-h5py
+'''
+import h5py
+
+model_json = model.to_json()
+with open('model.json' , 'w') as json_file:
+	json_file.write(model_json)
+model.save_weights('model.h5')
+print('Saved model to disk')
+
+#Load model and weights
+with open('model.json' , 'r') as json_file:
+	json = json_file.read()
+load_model = keras.models.model_from_json(json)
+load_model.load_weights('model.h5')
+print('Loaded model from disk')
+
+#Evaluate loaded model
+load_model.compile(keras.optimizers.Adam() , loss = 'sparse_categorical_crossentropy' , metrics = ['accuracy'])
+score = load_model.evaluate(test_x , test_y , verbose = 0)
+print('Test accuracy:' , score[1])
+
+#Prediction
+#prediction = model.predict_classes(numpy.array([[130 , 6.0 , 8.2 , 0.71]]))
+print(prediction)
+'''
+
+#Tendor Board
+'''
+Important parameters to view:
+	1. Loss over epoch
+	2. Accuracy over epoch
+
+'''
+
+#The Shape of data
+'''			Dataset Shape
+			(5 , 4)						<-- 5 examples each having 4 features
+			   |
+			   ٧
+			Layer Output Shape	Number of Parameters	<-- Number of trainable wegihts (weights coming from previous layer + 1 bias weight for each node)
+input_shape:		(4    ,  )					<-- 4 is the number of features in the dataset for each example
+Dense 1:	3 Nodes	(None , 3)		15			<-- Dense layer 1 and the input_shape are coded in the same line in KERAS
+Dropout 1:		//			0			<-- Randomly freez 20% of the nodes every epoch to reduce overfitting and get better generalisation
+Dense 2:	3 Nodes	(None , 3)		12			<-- None refers to any value in this position , 3 for 3 nodes
+Dropout 2:		//			0			<-- Best to use dropout layers between each layer
+Dense 3:	2 Nodes	(None , 2)		8			<-- 2 for 2 output nodes (each node for each class)
+'''
---
+++ Machine Learning/RNN.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import numpy , random , keras , pandas , sklearn
+
+#Import data
+data = pandas.read_csv('fruits.csv')																					#Import .csv dataset
+X = pandas.DataFrame.as_matrix(data[['mass' , 'width', 'height' , 'color_score']])										#Convert to numpy array
+examples = X.shape[0]
+features = X.shape[1]
+timestep = 1
+X = X.reshape((examples , timestep , features))
+n_featu = X.shape[2]
+Y = pandas.DataFrame.as_matrix(data['fruit_label'])																		#Convert to numpy array
+n_class = 4
+X , Y = sklearn.utils.shuffle(X , Y , random_state = 0)																	#Shuffle the dataset to make sure similar instances are not clustering next to each other
+
+#Setup neural network
+model = keras.models.Sequential()
+model.add(keras.layers.LSTM(3 , input_shape = (timestep , n_featu) , activation = 'relu' , return_sequences = True))
+model.add(keras.layers.LSTM(3 , activation = 'relu' , dropout = 0.25 , recurrent_dropout = 0.25))
+model.add(keras.layers.Dense(n_class , activation = 'softmax'))															#Output layer with 4 nodes (because of 4 classes) and the softmax activation function
+
+#Compile model
+model.compile(keras.optimizers.Adam(lr = 0.01) , loss = 'sparse_categorical_crossentropy' , metrics = ['accuracy'])				#Compile the model, identify here the loss function, the optimiser (Adam Gradient Descent with a Learning Rate of 0.01), and the evaluation metric
+
+#Train model
+model.fit(X , Y , batch_size = 8 , epochs = 50 , verbose = 2 , validation_split  = 0.25)								#Preform the network training, input the training feature and class tensors, identify the batch size (conventional to use multiples of 8 - 8 , 16 , 32 etc...), and the epoch number. Verbose 23 is best to printout the epoch cycle only. The validation split is splitting the dataset into a train/test set (0.25 = 25% for the test set), thus the final accuracy we want to use is the valication accuracy (where it is measured using the test set's preformace on the model)
+
+#Prediction
+PRE = numpy.array([[130 , 6.0 , 8.2 , 0.71]])
+PRE = PRE.reshape((PRE.shape[0] , timestep , PRE.shape[1]))
+prediction = model.predict_classes(PRE)
+print(prediction)
+
+'''
+keras.layers.Dropout(rate = 0.5)																						#Percentage of nodes (of the layer after it) to be randomly switched off (their weight not updated) during an epoch, this is to prevent over fitting and allow other neurones to learn multiple features. 0.5 = 50%
+keras.layers.reshape((58 , 1 , 4))																						#Reshapes a tensor into a new shape, for RNN that would be (examples , timestep of 1 , number of features)
+keras.layers.LSTM(units = 2 , activation = 'relu' , return_sequences = True)											#For RNN, return_sequences = True so that the layers take into account the timesteps (learn a sequence)
+
+The timestep is an extra dimention that allows weights from the previous epoch to be represented in the current epoch, thus datasets in sequence can be generalised
+Divide the dataset into timesteps:
+X = X.reshape((int(examples/timestep) , timestep , features))
+					  X has 5 examples each with 4 features and a timestep of 1
+					  |
+					  ٧
+LSTM1 (3 nodes)		(5,1,4)	<--- input_shape = (1,4) 1 timestep, and 1 weights for 1 feature therefore 4 weights
+					  |
+					  ٧
+LSTM2 (3 nodes)		(5,1,3)	<--- 3 features because of 3 nodes, same number of examples, therefore input_shape = (3,) 1 weights for 1 feature, therefore 3 weights. Timestep remains at 1.
+					  |
+					  ٧
+LSTM3 (3 nodes)		(5,1,3)	<--- 3 features because of 3 nodes, same number of examples, therefore input_shape = (3,) 1 weights for 1 feature, therefore 3 weights. Timestep remains at 1.
+					  |
+					  ٧
+Dense4 (4 nodes)	(5,1,3)	<--- 3 features because of 3 nodes, same number of examples, therefore input_shape = (3,) 1 weights for 1 feature, therefore 3 weights. Timestep remains at 1.
+					  |
+					  ٧
+					(5,1,4)	<--- 1 node for 1 class in Y, therefore 4 output values for 4 different classes (the probability of each class), this is compaired to the Y classes from the dataset to see if we get it right or wrong (the loss function). (examples , timestep , classes)
+Divide the dataset into a timestep of 1 to be able to make a predition on an LSTM:
+PRE = numpy.array([[130 , 6.0 , 8.2 , 0.71]])
+PRE = PRE.reshape((PRE.shape[0] , timestep , PRE.shape[1]))
+'''
