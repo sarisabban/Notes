4ecc6530a115cbf0461bbcfb95062c84b66925b1
acresearch <sari.sabban@gmail.com>
2018-05-04 12:05:28 +0300

    study updates

--- Machine Learning/RecurrentNN.py
+++ Machine Learning/RecurrentNN.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #https://github.com/keras-team/keras/blob/master/examples/lstm_text_generation.py
-import sys , numpy , random , keras
+import sys , numpy , random , keras , io
 
 #Import text
 text = open('../OLD/nietzsche.txt').read().lower()
@@ -22,22 +22,15 @@ for i in range(0 , len(text) - maxlen , step):			#Length of text - 40 and move 3
 	sentences.append(text[i : i + maxlen])			#Slice this range into an item and append it to the sentences list
 	next_chars.append(text[i + maxlen])			#Take the "next charachter" that comes after the sentace in the previous list and append it into a new list. That way we have a list of sentances and another list of charachters that come after each sentance
 
-
-
-
-
-
-#Vectorise
+#Vectorise - The dataset now has the shape (number of sentances , Maximum sentance length , number of available charachters)
 X = numpy.zeros((len(sentences) , maxlen , len(chars)) , dtype = numpy.bool)
 Y = numpy.zeros((len(sentences) , len(chars)) , dtype = numpy.bool)
+
+#One-hot encoding - True for the charachter that comes after the sequence
 for i , sentence in enumerate(sentences):
 	for t , char in enumerate(sentence):
 		X[i , t , chars_indices[char]] = 1
 	Y[i , chars_indices[next_chars[i]]] = 1
-
-
-
-
 
 #Setup neural network
 model = keras.models.Sequential()
@@ -45,10 +38,10 @@ model.add(keras.layers.LSTM(128 , input_shape = (maxlen , len(chars))))
 model.add(keras.layers.Dense(len(chars) , activation = 'softmax'))
 
 #Compile model
-model.compile(keras.optimizers.Adam(lr = 0.01) , loss = 'categorical_crossentropy' , metrics = ['accuracy'])
+model.compile(keras.optimizers.RMSprop(lr = 0.01) , loss = 'categorical_crossentropy' , metrics = ['accuracy'])
 model.summary()
 
-#Train model
+#Train model - Accuracy is not that important in NLP because it is relative. What is more important is the language output
 tensorboard = keras.callbacks.TensorBoard(log_dir = './logs')
 #model.fit(X , Y , batch_size = 128 , verbose = 2 , epochs = 60 , callbacks = [tensorboard])
 
@@ -111,4 +104,4 @@ def on_epoch_end(epoch , logs):
 
 
 
-
+#model.fit(x, y,batch_size=128,epochs=60,callbacks=[print_callback])
