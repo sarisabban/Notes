2985f4c93ce074b9be10b15780b00b992fc89180
Sari Sabban <sari.sabban@gmail.com>
2018-06-02 22:23:51 +0300

    Update GAN.py

--- Machine Learning/GAN.py
+++ Machine Learning/GAN.py
@@ -57,16 +57,15 @@ for epoch in range(epochs):
 	#Generate a fake image
 	images_train = X[numpy.random.randint(0, X.shape[0], size=batch_size)]	# Generate a real image tensor with shape (batch size, row, column, channel)
 	noise = numpy.random.uniform(-1.0, 1.0, size=[batch_size, 100])		# Generate a noise tensor with shape (Batch size, length of noise vector[100]) and fill it with random numbers between -1 and 1
-	images_fake = G.predict(noise)						# Use noise tensor to generate a prediction from the Generator neural network, the output shape of course will be (batch size, row, columns, channel)
+	images_fake = G.predict(noise)						# Use noise tensor to generate a prediction (batch size of fake images) from the Generator neural network, the output shape of course will be (batch size, row, columns, channel)
 	#Train discriminator
-	x = numpy.concatenate((images_train, images_fake))			# Combine the train tensor (from the dataset) with the fake tensor (from the generator) to generate a dataset with training examples and fake examples
-	y = numpy.ones([2*batch_size, 1])					# Generate the labels tensor with 2x the size of the batch (because we combines two batches train and fake) and give them all the label of 1 (True)
-	y[batch_size:, :] = 0							#?Turn all 1 values into 0? why?
+	x = numpy.concatenate((images_train, images_fake))			# Combine the real image tensor (from the dataset) with the fake images tensor (from the generator) to generate a dataset with real images and fake images
+	y = numpy.ones([2*batch_size, 1])					# Generate the labels tensor with twice the size of the batch (because we combined two batches real and fake) and give them all the label of 1 (True)
+	y[batch_size:, :] = 0							# For real images keep 1 (True) but for fake images replace 1 with 0 (False)
 	d_loss = DM.train_on_batch(x, y)					# Feed the training (real+fake) and label sets into the Discriminator neural network to train it to find the difference between fake and real data
-	y = numpy.ones([batch_size, 1])						#?Generate a new labels tensor and give it all the label of 1 (True)
-	noise = numpy.random.uniform(-1.0, 1.0, size=[batch_size, 100])		#?Generate another noise tensor with shape (Batch size, length of noise vector[100]) and fill it with random numbers between -1 and 1
-	#Train GAN
-	a_loss = AM.train_on_batch(noise, y)					#?Train Adversarial neural network, what happens here? the generator gets trained?
+	#Train adversarial
+	y = numpy.ones([batch_size, 1])						# After the Discriminator is trained, now generate a new labels tensor and give it all the label of 1 (True)
+	a_loss = AM.train_on_batch(noise, y)					# Train Adversarial neural network, can the discriminator find the fake image from the noise or not? if yes update weights of generator to become better at faking images
 	D_loss = round(float(d_loss[0]), 3)
 	D_accu = round(float(d_loss[1]), 3)
 	A_loss = round(float(a_loss[0]), 3)
