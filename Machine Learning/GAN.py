45a49c5aa6a172e655cb201e5cb8db0c572c63bf
Sari Sabban <sari.sabban@gmail.com>
2018-06-02 21:43:30 +0300

    Update GAN.py

--- Machine Learning/GAN.py
+++ Machine Learning/GAN.py
@@ -1,124 +1,92 @@
 #!/usr/bin/python3
 #https://github.com/eriklindernoren/Keras-GAN#gan
+#https://towardsdatascience.com/gan-by-example-using-keras-on-tensorflow-backend-1a6d515a60d0
 
-import keras
 import matplotlib.pyplot
 import numpy
+import keras
 
+#Import Data
 from keras.datasets import mnist
+(X, _), (_, _) = mnist.load_data()						# Import data
+X = X / 127.5 - 1.								# Make all tensor values between -1 and 1
+X = numpy.expand_dims(X, axis=3)						# Add an extra dimention for a channel
+img_shape = (28, 28, 1)								# Identify shape of data
+latent_dim = 100								# Size of noise
+batch_size = 32
+epochs = 30000
 
-img_rows = 28
-img_cols = 28
-channels = 1
-img_shape = (img_rows, img_cols, channels)
-latent_dim = 100
+#Discriminator
+D = keras.models.Sequential()
+D.add(keras.layers.Flatten(input_shape=img_shape))				# For am image flatten the tensor into a vector
+D.add(keras.layers.Dense(512))
+D.add(keras.layers.advanced_activations.LeakyReLU(alpha=0.2))
+D.add(keras.layers.Dense(256))
+D.add(keras.layers.advanced_activations.LeakyReLU(alpha=0.2))
+D.add(keras.layers.Dense(1, activation='sigmoid'))				# Output is either True or False
+D.summary()
 
-optimizer = keras.optimizers.Adam(0.0002, 0.5)
+#Generator
+G = keras.models.Sequential()
+G.add(keras.layers.Dense(256, input_dim=latent_dim))				# Input noise
+G.add(keras.layers.advanced_activations.LeakyReLU(alpha=0.2))
+G.add(keras.layers.BatchNormalization(momentum=0.8))
+G.add(keras.layers.Dense(512))
+G.add(keras.layers.advanced_activations.LeakyReLU(alpha=0.2))
+G.add(keras.layers.BatchNormalization(momentum=0.8))
+G.add(keras.layers.Dense(1024))
+G.add(keras.layers.advanced_activations.LeakyReLU(alpha=0.2))
+G.add(keras.layers.BatchNormalization(momentum=0.8))
+G.add(keras.layers.Dense(numpy.prod(img_shape), activation='tanh'))		# Output nodes for each data item in data vector
+G.add(keras.layers.Reshape(img_shape))						# Reshape output vector to shape of image
+G.summary()
 
-# Build and compile the discriminator
-model = keras.models.Sequential()
-model.add(keras.layers.Flatten(input_shape=img_shape))
-model.add(keras.layers.Dense(512))
-model.add(keras.layers.advanced_activations.LeakyReLU(alpha=0.2))
-model.add(keras.layers.Dense(256))
-model.add(keras.layers.advanced_activations.LeakyReLU(alpha=0.2))
-model.add(keras.layers.Dense(1, activation='sigmoid'))
-model.summary()
-img = Input(shape=img_shape)
-validity = model(img)
-model.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
+#Discriminator Model
+DM = keras.models.Sequential()
+DM.add(D)
+DM.compile(loss='binary_crossentropy', optimizer=keras.optimizers.RMSprop(lr=0.0008, clipvalue=1.0, decay=6e-8), metrics=['accuracy'])
 
-# Build the generator
-model = keras.models.Sequential()
-model.add(keras.layers.Dense(256, input_dim=latent_dim))
-model.add(keras.layers.advanced_activations.LeakyReLU(alpha=0.2))
-model.add(keras.layers.BatchNormalization(momentum=0.8))
-model.add(keras.layers.Dense(512))
-model.add(keras.layers.advanced_activations.LeakyReLU(alpha=0.2))
-model.add(keras.layers.BatchNormalization(momentum=0.8))
-model.add(keras.layers.Dense(1024))
-model.add(keras.layers.advanced_activations.LeakyReLU(alpha=0.2))
-model.add(keras.layers.BatchNormalization(momentum=0.8))
-model.add(keras.layers.Dense(numpy.prod(img_shape), activation='tanh'))
-model.add(keras.layers.Reshape(img_shape))
-model.summary()
-noise = Input(shape=(latent_dim,))
-img = model(noise)
-
-# The generator takes noise as input and generates imgs
-z = Input(shape=(100,))
-img = generator(z)
-
-# For the combined model we will only train the generator
-discriminator.trainable = False
-
-# The discriminator takes generated images as input and determines validity
-validity = discriminator(img)
-
-# The combined model  (stacked generator and discriminator)
-# Trains the generator to fool the discriminator
-combined = keras.models.Model(z, validity)
-combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+#Adversarial Model
+AM = keras.models.Sequential()
+AM.add(G)
+AM.add(D)
+AM.compile(loss='binary_crossentropy', optimizer=keras.optimizers.RMSprop(lr=0.0004, clipvalue=1.0, decay=3e-8), metrics=['accuracy'])
 
 #Training
-epochs = 3
-batch_size = 32
-sample_interval = 200
-# Load the dataset
-(X_train, _), (_, _) = mnist.load_data()
-
-# Rescale -1 to 1
-X_train = X_train / 127.5 - 1.
-X_train = numpy.expand_dims(X_train, axis=3)
-
-# Adversarial ground truths
-valid = numpy.ones((batch_size, 1))
-fake = numpy.zeros((batch_size, 1))
-
 for epoch in range(epochs):
+	#Generate a fake image
+	images_train = X[numpy.random.randint(0, X.shape[0], size=batch_size)]	# Generate a real image tensor with shape (batch size, row, column, channel)
+	noise = numpy.random.uniform(-1.0, 1.0, size=[batch_size, 100])		# Generate a noise tensor with shape (Batch size, length of noise vector[100]) and fill it with random numbers between -1 and 1
+	images_fake = G.predict(noise)						# Use noise tensor to generate a prediction from the Generator neural network, the output shape of course will be (batch size, row, columns, channel)
+	#Train discriminator
+	x = numpy.concatenate((images_train, images_fake))			# Combine the train tensor (from the dataset) with the fake tensor (from the generator) to generate a dataset with training examples and fake examples
+	y = numpy.ones([2*batch_size, 1])					# Generate the labels tensor with 2x the size of the batch (because we combines two batches train and fake) and give them all the label of 1 (True)
+	y[batch_size:, :] = 0							#?Turn all 1 values into 0? why?
+	d_loss = DM.train_on_batch(x, y)					# Feed the training (real+fake) and label sets into the Discriminator neural network to train it to find the difference between fake and real data
+	y = numpy.ones([batch_size, 1])						#?Generate a new labels tensor and give it all the label of 1 (True)
+	noise = numpy.random.uniform(-1.0, 1.0, size=[batch_size, 100])		#?Generate another noise tensor with shape (Batch size, length of noise vector[100]) and fill it with random numbers between -1 and 1
+	#Train GAN
+	a_loss = AM.train_on_batch(noise, y)					#?Train Adversarial neural network, what happens here? the generator gets trained?
+	D_loss = round(float(d_loss[0]), 3)
+	D_accu = round(float(d_loss[1]), 3)
+	A_loss = round(float(a_loss[0]), 3)
+	print ('{} [D loss: {}, accuracy: {}] [G loss: {}]'.format(epoch, D_loss, D_accu, A_loss))
 
-	# ---------------------
-	#  Train Discriminator
-	# ---------------------
 
-	# Select a random batch of images
-	idx = numpy.random.randint(0, X_train.shape[0], batch_size)
-	imgs = X_train[idx]
 
-	noise = numpy.random.normal(0, 1, (batch_size, 100))
-
-	# Generate a batch of new images
-	gen_imgs = generator.predict(noise)
-
-	# Train the discriminator
-	d_loss_real = discriminator.train_on_batch(imgs, valid)
-	d_loss_fake = discriminator.train_on_batch(gen_imgs, fake)
-	d_loss = 0.5 * numpy.add(d_loss_real, d_loss_fake)
-
-	# ---------------------
-	#  Train Generator
-	# ---------------------
-
-	noise = numpy.random.normal(0, 1, (batch_size, 100))
-
-	# Train the generator (to have the discriminator label samples as valid)
-	g_loss = combined.train_on_batch(noise, valid)
-
-	# Plot the progress
-	print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
 
 #Generate Image
-r, c = 5, 5
-noise = numpy.random.normal(0, 1, (r * c, 100))
-gen_imgs = generator.predict(noise)
-# Rescale images 0 - 1
-gen_imgs = 0.5 * gen_imgs + 0.5
-fig, axs = matplotlib.pyplot.subplots(r, c)
-cnt = 0
-for i in range(r):
-	for j in range(c):
-		axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
-		axs[i,j].axis('off')
-		cnt += 1
-fig.savefig("%d.png" % int(epoch))
-matplotlib.pyplot.close()
+r, c = 5, 5									# 
+noise = numpy.random.normal(0, 1, (r * c, 100))					# 
+gen_imgs = G.predict(noise)							# Generate a image (predition) from the neural network
+gen_imgs = 0.5 * gen_imgs + 0.5							# Rescale images 0 - 1
+fig, axs = matplotlib.pyplot.subplots(r, c)					# 
+cnt = 0										# 
+for i in range(r):								# 
+	for j in range(c):							# 
+		axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')		# 
+		axs[i,j].axis('off')						# 
+		cnt += 1							# 
+fig.savefig('generated.png')							# Save image
+matplotlib.pyplot.close()							# Close plot
+from IPython.display import Image ; Image("generated.png")			# Show Image in Jupyter Notebook
