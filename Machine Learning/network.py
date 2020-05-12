b90d730992c5259f65f4b4c9c3e9d44e36946073
Sari Sabban <sari.sabban@gmail.com>
2020-05-12 06:13:35 +0300

    Update network.py

--- Machine Learning/network.py
+++ Machine Learning/network.py
@@ -62,6 +62,11 @@ dw3 = np.dot(y2.T, dZ3)/m							# Derivative of w of layer_3 (output layer)
 print('\ndw3', dw3.shape, '\n', dw3)
 db3 = (np.sum(dZ3, axis=0, keepdims=True))/m		# Derivative of b of layer_3 (output layer) keep dimentions otherwise can become scaler rather than (1,1) matrix, and sum the 0th axis (squeez top to bottom)
 print('\nbd3 =', db3.shape, '\n', db3)
+### --- GRADIENT DECSCENT --- ###
+w3 = np.subtract(w3, a*dw3)							# Update w for layer_3
+print('\nw3', w3.shape ,'\n', w3)
+b3 = b3 - a*db3										# Update b for layer_3
+print('\nb3', b3.shape, '\n', b3)
 
 ## Layer 2
 dy2 = (-Y/y2)+((1-Y)/(1-y2))						# Derivative of layer_2 activation function
@@ -72,6 +77,11 @@ dw2 = np.dot(y1.T, dZ2)/m							# Derivative of w of layer_2
 print('\ndw2', dw2.shape, '\n', dw2)
 db2 = (np.sum(dZ2, axis=0, keepdims=True))/m		# Derivative of b of layer_2
 print('\nbd2 =', db2.shape, '\n', db2)
+### --- GRADIENT DECSCENT --- ###
+w2 = np.subtract(w2, a*dw2)							# Update w for layer_2
+print('\nw2', w2.shape ,'\n', w2)
+b2 = b2 - a*db2										# Update b for layer_2
+print('\nb2', b2.shape, '\n', b2)
 
 ## Layer 1
 dy1 = (-Y/y1)+((1-Y)/(1-y1))						# Derivative of layer_1 activation function 
@@ -82,20 +92,7 @@ dw1 = np.dot(X.T, dZ1)/m							# Derivative of w of layer_1
 print('\ndw1', dw1.shape, '\n', dw1)
 db1 = (np.sum(dZ1, axis=0, keepdims=True))/m								# Derivative of b of layer_1
 print('\nbd1 =', db1.shape, '\n', db1)
-
-print('-----')
-
 ### --- GRADIENT DECSCENT --- ###
-w3 = np.subtract(w3, a*dw3)							# Update w for layer_3
-print('\nw3', w3.shape ,'\n', w3)
-b3 = b3 - a*db3										# Update b for layer_3
-print('\nb3', b3.shape, '\n', b3)
-
-w2 = np.subtract(w2, a*dw2)							# Update w for layer_2
-print('\nw2', w2.shape ,'\n', w2)
-b2 = b2 - a*db2										# Update b for layer_2
-print('\nb2', b2.shape, '\n', b2)
-
 w1 = np.subtract(w1, a*dw1)							# Update w for layer_1
 print('\nw1', w1.shape ,'\n', w1)
 b1 = b1 - a*db1										# Update b for layer_1
