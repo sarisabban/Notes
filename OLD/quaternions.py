bbf8a8e977a82fab088c1cc6dfd50ce305174a60
Sari Sabban <sari.sabban@gmail.com>
2022-12-23 22:02:05 -0500

    Update quaternions.py

--- OLD/quaternions.py
+++ OLD/quaternions.py
@@ -44,8 +44,20 @@ def rotate(v, n, theta):
 	result = np.array(v)*C + nxv*S
 	return(result)
 
+def rodrigues(v, n, theta):
+	''' Rotate a vector around any axis using Rodrigues Rotation Formula '''
+	v = np.array(v)
+	n = n / np.linalg.norm(n)
+	theta = math.radians(theta)
+	C = math.cos(theta)
+	S = math.sin(theta)
+	nxv = np.cross(n, v)
+	ndv = n*(np.dot(n, v))
+	result = ndv*(1-C) + v*C + nxv*S
+	return(result)
+
 def qrotate(v, n, theta):
-	''' Rotate a vector around axis n using quaternions'''
+	''' Rotate a vector around axis n using quaternions '''
 	e = math.e
 	theta = math.radians(theta)
 	q = np.array([0, v[0], v[1], v[2]])
@@ -59,12 +71,17 @@ def qrotate(v, n, theta):
 
 
 
+
+
 v = [0, 0, 1]
 n = [0, 1, 0]
 theta = 90
 
-q_ = rotate(v, n, theta)
-print(q_)
+#v_ = rotate(v, n, theta)
+#print(v_)
 
-q_ = qrotate(v, n, theta)
-print(q_)
+#q_ = qrotate(v, n, theta)
+#print(q_)
+
+#v_ = rodrigues(v, n, theta)
+#print(v_)
