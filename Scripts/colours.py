6e4a38c2de57f07013a956c01999553325d212ba
Sari Sabban <sari.sabban@gmail.com>
2025-03-27 18:53:07 +0300

    Add files via upload

---
+++ Scripts/colours.py
@@ -0,0 +1,11 @@
+Red    = '\033[31m'
+Green  = '\033[32m'
+Blue   = '\033[34m'
+Organe = '\033[33m'
+Grey   = '\033[90m'
+White  = '\033[97m'
+Purple = '\033[35m'
+Reset  = '\033[39m'
+
+C = White
+print(f'xxx{C}COLOUR{Reset}xxx')
